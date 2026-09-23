c5ff884757b30e7400106738ee19eafebeb63bc0
Asim Ahmed <132520640+asim-aa@users.noreply.github.com>
2026-09-23 16:37:34 -0700

    Remove EDS 124 page from professional portfolio

--- teaching-content.js
+++
@@ -1,7 +0,0 @@
-/* Add real EDS 124 work here as it becomes available. See README.md for field examples. */
-const teachingContent = {
-  projects: [],
-  videos: [],
-  reflections: [],
-  resources: []
-};
